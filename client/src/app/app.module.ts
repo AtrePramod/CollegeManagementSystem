ea1af37d583b28fe292e438e657d5ffcc6d9c658
Atre Pramod Namdev <pramodatre05@gmail.com>
2024-01-19 20:41:01 +0530

    login page created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './user/profile/profile.component';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
