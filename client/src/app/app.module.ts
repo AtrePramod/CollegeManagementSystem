d4ccb3979d7fbdd9a5849af32efba4cc69d1f517
Atre Pramod Namdev <pramodatre05@gmail.com>
2024-01-19 17:59:45 +0530

    set all module to components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
